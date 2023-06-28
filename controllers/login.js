9e549a29fec1d0facc5cce54bd1c1b59b28cbea0
Jari Hiekkapelto <jari@rnd.works>
2023-06-28 11:10:43 +0300

    migrations

--- controllers/login.js
+++ controllers/login.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 
 const { SECRET } = require('../util/config')
 const User = require('../models/user')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (request, response) => {
   const body = request.body
@@ -21,6 +22,12 @@ router.post('/', async (request, response) => {
     })
   }
 
+  if (user.disabled) {
+    return response.status(401).json({
+      error: 'account disabled, please contact admin'
+    })
+  }
+
   const userForToken = {
     username: user.username,
     id: user.id,
@@ -31,6 +38,30 @@ router.post('/', async (request, response) => {
   response
     .status(200)
     .send({ token, username: user.username, name: user.name })
+})
+
+const isAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  if (!user.admin) {
+    return res.status(401).json({ error: 'operation not permitted' })
+  }
+  next()
+}
+
+router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username
+    }
+  })
+
+  if (user) {
+    user.disabled = req.body.disabled
+    await user.save()
+    res.json(user)
+  } else {
+    res.status(404).end()
+  }
 })
 
 module.exports = router
